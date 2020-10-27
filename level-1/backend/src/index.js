e2c5c7b0982e424666b9bc8444a15643be39c85a
Andre <andredelima92@gmail.com>
2020-10-26 21:38:22 -0300

    Simulating a database with an array

--- level-1/backend/src/index.js
+++ level-1/backend/src/index.js
@@ -1,27 +1,61 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/projects", (req, res) => {
-  const query = req.query;
+const projects = [];
 
-  return res.json(query);
+app.get("/projects", (req, res) => {
+  const { title } = req.query;
+
+  const result = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return res.json(result);
 });
 
 app.post("/projects", (req, res) => {
-  return res.json(req.body);
+  const { title, owner } = req.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return res.json(project);
 });
 
 app.put("/projects/:id", (req, res) => {
-  const params = req.params;
+  const { id } = req.params;
+  const { title, owner } = req.body;
 
-  return res.json(params);
+  const projectIndex = projects.findIndex((project) => project.id == id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: "Project not found." });
+  }
+
+  const project = { id, title, owner };
+
+  projects[projectIndex] = project;
+
+  return res.json(project);
 });
 
 app.delete("/projects/:id", (req, res) => {
-  return res.json(["Projeto 2", "Projeto 3"]);
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: "Project not found." });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.listen(3333, () => {
