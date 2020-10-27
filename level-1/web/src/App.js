01fd2b32d9c634a075670d8e5aa9bf0d0ed288a4
Andre <andredelima92@gmail.com>
2020-10-27 08:56:21 -0300

    create react application manually

---
+++ level-1/web/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h2>teste de titulo 1</h2>
+    </>
+  );
+}
+
+export default App;
