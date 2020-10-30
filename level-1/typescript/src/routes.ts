3c4ed9aa8eaa37cfe79e05a51a8a049056bbed05
Andre <andredelima92@gmail.com>
2020-10-29 22:00:00 -0300

    types

--- level-1/typescript/src/routes.ts
+++ level-1/typescript/src/routes.ts
@@ -2,7 +2,17 @@ import { Response, Request } from "express";
 import createUser from "./services/CreateUser";
 
 export function helloWorld(req: Request, res: Response) {
-  const user = createUser("Andre", "andre@teste.com", 123456);
+  const user = createUser({
+    email: "andre@teste",
+    password: 123456,
+    techs: [
+      "Node.js",
+      "React",
+      "Laravel",
+      { title: "javascript", experience: 100 },
+    ],
+    schools: ["Escola1", "Escola2"],
+  });
 
   return res.json({ message: "Hello World" });
 }
--- level-1/typescript/src/services/CreateUser.ts
+++ level-1/typescript/src/services/CreateUser.ts
@@ -1,4 +1,21 @@
-export default function createUser(name = "", email: string, password: number) {
+interface TechInterface {
+  title: string;
+  experience: number;
+}
+
+interface CreateUserInterface {
+  name?: string;
+  email: string;
+  password: number;
+  techs: Array<string | TechInterface>;
+  schools: string[];
+}
+
+export default function createUser({
+  name = "",
+  email,
+  password,
+}: CreateUserInterface) {
   const user = { name, email, password };
 
   return user;
