41a0edf97c8efa1f46ebbfdabce7227439684149
Andre <andredelima92@gmail.com>
2020-11-02 15:12:31 -0300

    init projeroject node level-2

---
+++ level-2/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.json({ message: "hello world" });
+});
+
+app.listen(3333, () => {
+  console.log("server is online in 3333");
+});
